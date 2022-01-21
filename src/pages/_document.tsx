30b899d0c7dc92b887b2c6facd3c835b956d94d9
dulranga <dulranga.webdesign@gmail.com>
2022-01-21 16:18:14 +0530

    added global configurations

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
