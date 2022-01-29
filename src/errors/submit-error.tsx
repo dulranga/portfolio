048fbf252f7171601bc8ef345fe3c43f7c6332ec
dulranga <dulranga.webdesign@gmail.com>
2022-01-29 16:39:09 +0530

    Created Submit error component

---
+++ src/errors/submit-error.tsx
@@ -0,0 +1,48 @@
+import { Close } from "@material-ui/icons";
+import { AnimatePresence, motion, useCycle, Variants } from "framer-motion";
+import { FC, useEffect, useState } from "react";
+import styles from "./errors.module.scss";
+
+const variants: Variants = {
+  initial: {
+    opacity: 0.5,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    opacity: 0,
+    y: -100,
+  },
+};
+export interface SubmitErrorProps {
+  show: boolean;
+  message?: string;
+  close?: () => void;
+}
+
+const SubmitError: FC<SubmitErrorProps> = ({ show, message, close }) => {
+  return (
+    <AnimatePresence initial={false} exitBeforeEnter>
+      {show && (
+        <motion.div
+          variants={variants}
+          initial="initial"
+          exit="exit"
+          key={"submit-error"}
+          animate="animate"
+          className={[styles.submit_error, styles.error].join(" ")}
+        >
+          <span>{message}</span>
+          <button onClick={close}>
+            <Close fontSize="small" htmlColor="#fff" />
+          </button>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default SubmitError;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,71 +1,23 @@
-import Input, { InputProps } from "@components/input";
+import Input from "@components/input";
 import { DownloadResume } from "@components/intro";
-import {
-  Description,
-  Mail,
-  Person,
-  Title,
-  ViewQuilt,
-} from "@material-ui/icons";
+import contactFields from "@constants/contact-details";
+import SubmitError, { SubmitErrorProps } from "@errors/submit-error";
 import styles from "@styles/pages.module.scss";
+import axios from "axios";
 import Head from "next/head";
 import { FC, FormEvent, useState } from "react";
 
 interface ContactPageProps {}
 
-const fields = [
-  {
-    label: "What's your name?",
-    id: "name",
-    icon: <Person />,
-    inputProps: {
-      placeholder: "John snow",
-    },
-  },
-  {
-    label: "What's your email?",
-    id: "email",
-    icon: <Mail />,
-    inputProps: {
-      placeholder: "john@westeros.com",
-    },
-  },
-  {
-    label: "Project title?",
-    id: "title",
-    icon: <Title />,
-    inputProps: {
-      placeholder: "What's your amazing website is about",
-    },
-  },
-  {
-    label: "Has a UI?",
-    id: "ui",
-    icon: <ViewQuilt />,
-    inputProps: {
-      placeholder: "That's awesome if you got a UI already",
-      type: "file",
-    },
-  },
-  {
-    label: "A brief description",
-    id: "description",
-    icon: <Description />,
-    inputElement: <textarea cols={10} rows={5} />,
-    inputProps: {
-      placeholder: "my website is all about ...",
-    },
-  },
-] as const;
-
-const getKeys = () => fields.map((field) => field.id);
+const getKeys = () => contactFields.map((field) => field.id);
 
 type Key = ReturnType<typeof getKeys>[number];
 type Info = {
-  [key in Key]?: any;
+  [key in Key]: string | Blob;
 };
 
 const ContactPage: FC<ContactPageProps> = () => {
+  const [error, setError] = useState<SubmitErrorProps>({ show: false });
   const [info, setInfo] = useState<Info>({
     name: "",
     description: "",
@@ -74,13 +26,35 @@ const ContactPage: FC<ContactPageProps> = () => {
     ui: "",
   });
 
-  const updateInfo = (key: Key) => (value: any) => {
+  const updateInfo = (key: Key) => (value: string | Blob) => {
     setInfo((prev) => ({ ...prev, [key]: value }));
   };
 
-  const submitInfo = (e: FormEvent<HTMLFormElement>) => {
+  const submitInfo = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const fd = new FormData();
+
+    Object.keys(info).forEach((key) => {
+      fd.append(key, info[key]);
+    });
+
+    try {
+      // const req = await backend.post("contact", new FormData(e.target));
+      const req = await axios({
+        method: "POST",
+        url: "http://localhost:8000/contact.php",
+        data: fd,
+      });
+
+      const res = await req.data;
+
+      if (res.error) setError({ show: true, message: res.error });
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
     <main className={styles.contact_page}>
       <Head>
@@ -94,7 +68,7 @@ const ContactPage: FC<ContactPageProps> = () => {
 
       <div className="content">
         <form onSubmit={submitInfo}>
-          {fields.map((field, i) => (
+          {contactFields.map((field, i) => (
             <Input
               key={field.id}
               delay={i * 0.05}
@@ -110,6 +84,7 @@ const ContactPage: FC<ContactPageProps> = () => {
           </div>
         </form>
       </div>
+      <SubmitError {...error} close={() => setError({ show: false })} />
     </main>
   );
 };
